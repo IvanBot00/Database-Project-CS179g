63daab902aabdab4bda0577fe9d82f07fcebdb36
pjsrcool <justinpham79@outlook.com>
2021-11-13 01:07:52 -0800

    added search functionality

--- frontend/src/Forms/User.js
+++ frontend/src/Forms/User.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import axios from "axios";
 import {
   Form,
   Row,
@@ -6,16 +8,53 @@ import {
 } from 'react-bootstrap';
 
 function User() {
+  // const [request, updateRequest] = useState(false);
+  const [Name, updateName] = useState('');
+  const [Yelping_Since, updateYS] = useState("2000-01-01");
+  const [ReviewCountMin, updateReviewCountMin] = useState(0);
+  const [ReviewCountMax, updateReviewCountMax] = useState(100);
+  const [UsefulMin, updateUsefulMin] = useState(0);
+  const [UsefulMax, updateUsefulMax] = useState(100);
+  const [FunnyMin, updateFunnyMin] = useState(0);
+  const [FunnyMax, updateFunnyMax] = useState(100);
+  const [CoolMin, updateCoolMin] = useState(0);
+  const [CoolMax, updateCoolMax] = useState(100);
 
+  const getData = async (e) => {
+    e.preventDefault();
+
+    console.log("requesting user data");
+
+    let url = "http://localhost:8000/server/userSearch/";
+    const request =  {
+      params : {
+        name : Name,
+        yelping_since : Yelping_Since,
+        reviewCountMin : ReviewCountMin,
+        reviewCountMax : ReviewCountMax,
+        usefulMin : UsefulMin,
+        usefulMax : UsefulMax,
+        funnyMin : FunnyMin,
+        funnyMax : FunnyMax,
+        coolMin : CoolMin,
+        coolMax : CoolMax,
+      }
+    }
+    
+    const res = await axios.get(url, request);
+    console.log(res.data);
+}
 
   return (
-    <Form>
+    <Form onSubmit={getData}>
       <Row>
         <Col>
           <Form.Group>
             <Form.Label>Name</Form.Label>
             <Form.Control
               type='input'
+              value={Name}
+              onChange={(e) => updateName(e.target.value)}
             />
           </Form.Group>
         </Col>
@@ -24,6 +63,7 @@ function User() {
             <Form.Label>Profile Creation Date</Form.Label>
             <Form.Control
               type='date'
+              onChange={(e) => updateYS(e.target.value.format('YYYY-MM-DD'))}
             />
           </Form.Group>
         </Col>
