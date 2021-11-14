bb44821d2661e2674bb6f4589ba822e82b5b14a4
pjsrcool <jpham079@ucr.edu>
2021-11-13 17:57:10 -0800

    progress on business search

--- frontend/src/Pages/Businesses.js
+++ frontend/src/Pages/Businesses.js
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Businesses() {
+  const [serverReturns, updateServerReturns] = useState([]);
+  const [Address, updateAddress] = useState("");
+  const [BusinessName, updateBN] = useState("");
+  const [NumReviews, updateNumReviews] = useState("");
+  const [NumStars, updateNumStars] = useState(0.0);
+
+
+  const getData = async (e) => {
+    e.preventDefault();
+
+    console.log("requesting user data");
+
+    let url = "http://localhost:8000/server/userSearch/";
+    const request =  {
+      params : {
+        address : Address,
+        name : BusinessName,
+        review_count : NumReviews,
+        stars : NumStars
+      }
+    }
+    
+    const res = await axios.get(url, request);
+    console.log(res.data);
+}  
+
+
   return (
     <>
       <div className='page-content'>
