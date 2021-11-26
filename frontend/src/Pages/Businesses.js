f80619bb484ca37075890d350a27ea4159c1600c
Ubuntu <ubuntu@ip-172-31-16-7.us-east-2.compute.internal>
2021-11-26 18:42:45 +0000

    Add sentiment graph

--- frontend/src/Pages/Businesses.js
+++ frontend/src/Pages/Businesses.js
@@ -1,14 +1,16 @@
 import { BusinessSearchForm } from '../Forms';
+import BusinessSentiment from '../components/BusinessSentiment';
 
 function Businesses() {
   return (
     <>
       <div className='page-content'>
-        <h1>Reviews of Businesses with wifi vs no wifi</h1>
-      </div>
-      <div className='page-content'>
         <h3>Search Businesses</h3>
         <BusinessSearchForm />
+      </div>
+      <div className='page-content' style={{height: '500px'}}>
+        <h3>Sentiments for businesses by state</h3>
+        <BusinessSentiment/>
       </div>
     </>
   );
---
+++ frontend/src/components/BusinessSentiment.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ReferenceLine,
+  ResponsiveContainer,
+} from 'recharts';
+import data from './data/states.json';
+
+export default function BusinessSentiment() {
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   const url = 'http://localhost:8000/server/avgSentimentPerState/'
+  //   const res = axios.get(url);
+  //   console.log(res);
+  //   setData(res);
+  // }, []);
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        layout='vertical'
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis type='number' />
+        <YAxis type='category' dataKey="name" />
+        <Tooltip />
+        <Legend />
+        <ReferenceLine y={0} stroke="#000" />
+        <Bar dataKey="sentiment" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
